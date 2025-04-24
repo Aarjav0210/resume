529920a759797a7ea6a5e2fe8a443f366ae3c4e0
Aarjav0210 <Aarjav02@gmail.com>
2025-04-24 10:25:12 +0100

    Fix the transition of grid cols between sm and md for work experience

--- src/app/views/WorkExperienceView.tsx
+++ src/app/views/WorkExperienceView.tsx
@@ -52,7 +52,7 @@ export default function WorkExperienceView({ currentSection, setCurrentSection }
 
       {/* Scrollable cards with fades */}
       <FadingScroll className="row-start-2 rounded-[20px] mx-20 my-9" fadeHeight={50} backgroundColor="#171717">
-        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 mdlg:grid-cols-2 xl:grid-cols-3 gap-4">
           {entries.map((entry) => (
             <WorkExperienceCard
               key={entry.id}
