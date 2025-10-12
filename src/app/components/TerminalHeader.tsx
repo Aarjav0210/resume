205d9ac08a7a5a29254c41fe24481545d7dd4ac0
Aarjav0210 <Aarjav02@gmail.com>
2025-10-12 13:18:13 -0400

    Update brown card to highligh present study

--- src/app/components/TerminalHeader.tsx
+++ src/app/components/TerminalHeader.tsx
@@ -20,17 +20,7 @@ const {
 	className 
 } = props;
 
-	// Set default text sizes if not provided
-	// const defaultTextSize = {
-	// 	lg: 'text-6xl',
-	// 	md: 'text-5xl',
-	// 	sm: 'text-4xl',
-	// };
-
-	// const appliedTextSize = textSize || defaultTextSize;
-	
 	return (
-		// <h1 className={`lg:${appliedTextSize.lg} md:${appliedTextSize.md} sm:${appliedTextSize.sm} font-normal`}>
 		<h1 className={`text-2xl md:text-3xl lg:text-4xl font-normal ${className}`}>
 			({username}) <span className="text-[#4CF0E8]">-&gt;</span> <span className="text-[#84EF12]">~</span>{" "}
 			<span className='md:hidden'><br/>  </span>
